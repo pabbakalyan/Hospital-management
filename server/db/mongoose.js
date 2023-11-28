cbdbe42d7d69dcac6feb04fd2d4b3e6ecbe6778a
pabbakalyan <119713183+pabbakalyan@users.noreply.github.com>
2023-11-29 03:21:15 +0530

    Update mongoose.js

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -4,6 +4,7 @@ var mongoose = require ('mongoose');
 mongoose.Promise = global.Promise;
 
 //change the database with yours
-mongoose.connect("mongodb://admin:admin123@ds145220.mlab.com:45220/nhs-app");
+mongoose.connect('mongodb://username:password@ds145220.mlab.com:45220/database', { useNewUrlParser: true, useUnifiedTopology: true });
 
 module.exports = {mongoose};
+
